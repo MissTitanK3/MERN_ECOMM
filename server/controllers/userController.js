c48fc51b787e741facbc22e252d388f004905d49
MissTitanK3 <tianna.m.mccoy@gmail.com>
2021-09-02 15:12:49 -0700

    Added backend authentication. Login/regigster

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/genorateToken.js'
 
 // @desc   Auth user & get token
 // @route  POST /api/users/login
@@ -17,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
@@ -26,6 +27,66 @@ const authUser = asyncHandler(async (req, res) => {
 
 })
 
+// @desc   Get user profile
+// @route  GET /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  // To test error state
+  // throw new Error('Not Authorized')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User Not Found')
+  }
+
+})
+
+// @desc   Register new user profile
+// @route  POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  // To test error state
+  // throw new Error('Not Authorized')
+  const { email, password, name } = req.body
+  const userCheck = await User.findOne({ email: email })
+
+  if (userCheck) {
+    res.status(400)
+    throw new Error('User Already Exists')
+
+  }
+
+  const user = await User.create({
+    name, email, password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User Data')
+  }
+
+})
+
 export {
-  authUser
+  authUser,
+  getUserProfile,
+  registerUser
 }
